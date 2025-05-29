8e071321a6281058dc3b7c13993ef9fc5b706bf0
trent <tredeckard@gmail.com>
2025-05-28 20:40:35 -0600

    commit test

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "You are connected to a server!"
+    return "You are connected to a server! -- Test"
 
 if os.getenv("ENV") == "dev" :
     app.run(debug=True)
